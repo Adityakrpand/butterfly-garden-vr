e09805a47689699c7d7e8bb574abaa29921c2f10
Adityakrpand <adityakrpandey9889@gmail.com>
2026-06-25 21:53:34 +0530

    Sprint 1: Render first Three.js scene

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,22 @@ import { Canvas } from '@react-three/fiber'
 
 export default function Home() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Canvas>
-        <color attach="background" args={['black']} />
+    <main style={{ width: '100vw', height: '100vh' }}>
+      <Canvas camera={{ position: [0, 0, 5], fov: 60 }}>
+        <color attach="background" args={['#202020']} />
 
-        <ambientLight />
+        <ambientLight intensity={2} />
+
+        <directionalLight
+          position={[5, 5, 5]}
+          intensity={2}
+        />
 
         <mesh>
           <boxGeometry args={[1, 1, 1]} />
           <meshStandardMaterial color="orange" />
         </mesh>
       </Canvas>
-    </div>
+    </main>
   )
 }
